9ac730b3de009b7af189f7796c32258de52289c1
Capriatto <michu-1@hotmail.com>
2013-07-14 22:56:52 -0500

    changing send to clipboard, taking quotes

--- JavaFXApplication13/src/javafxapplication13/SampleController.java
+++ JavaFXApplication13/src/javafxapplication13/SampleController.java
@@ -4,11 +4,13 @@
  */
 package javafxapplication13;
 
+import java.awt.Toolkit;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.StringSelection;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.net.URL;
 import java.text.DecimalFormat;
@@ -35,14 +37,14 @@ import javafx.stage.FileChooser;
  * @author oky
  */
 public class SampleController implements Initializable {
-
+    
     ArrayList lineaTexto = new ArrayList();
     ArrayList tipoDato = new ArrayList();
     ArrayList separados = new ArrayList();//lineas sin delimitador
     String separador;
+    String textoGenerado;
     Boolean estadoIf = false;
     StringBuffer stringbufer = new StringBuffer();
-    ;
     int res;
     File file;
     @FXML // indica que se hace una referencia a los componentes del archivo FXML "Sample"
@@ -86,7 +88,7 @@ public class SampleController implements Initializable {
     String primeraLinea;
     int contador;
     int countTokens;
-
+    
     @FXML
     private void handleButtonAction(ActionEvent event) throws FileNotFoundException {
         cbPeso.setSelected(false);
@@ -96,14 +98,14 @@ public class SampleController implements Initializable {
         FileChooser chooser = new FileChooser();
         FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("TXT files y CSV files (*.txt, *.csv)", "*.txt", "*.csv");
         chooser.getExtensionFilters().add(extFilter);
-
+        
         try {
             file = chooser.showOpenDialog(null);
             lblpath.setStyle("-fx-font-weight: normal;");
             lblpath.setText(file.getPath());
             cbPeso.setDisable(false);
             cbCantidad.setDisable(false);
-        } catch (Exception e) {
+            } catch (Exception e) {
             System.out.println("no se pudo leer un archivo o no fue cargado.");
             lblpath.setStyle("-fx-font-weight: bold; -fx-text-fill: #F27954;");
             lblpath.setText("No ha elegido un archivo, o está dañado.");
@@ -111,7 +113,7 @@ public class SampleController implements Initializable {
             cbCantidad.setDisable(true);
         }
     }
-
+    
     @FXML
     public void readingtxt() throws FileNotFoundException, IOException {
         fr = new FileReader(file);
@@ -119,14 +121,14 @@ public class SampleController implements Initializable {
         int longLinea = 0; //longitud de la línea del txt
 
         for (int i = 0; i < tipoDato.size(); i++) {
-           String coincidencia=(String) tipoDato.get(i);
-           int  posicion= coincidencia.lastIndexOf("(");
-           String impresion= coincidencia.substring(0, posicion);
-            System.out.println(""+impresion);
+            String coincidencia = (String) tipoDato.get(i);
+            int posicion = coincidencia.lastIndexOf("(");
+            String impresion = coincidencia.substring(0, posicion);
+            System.out.println("" + impresion);
         }
         
         stringbufer.append("INSERT INTO ".concat(txtNombre.getText().trim()));
-        stringbufer.append(" ".concat("("));
+        stringbufer.append(" ".concat("( )").concat(" VALUES").concat("\n"));
         while ((sCadena = bf.readLine()) != null) {
             lineaTexto.add(sCadena);
         }
@@ -143,15 +145,15 @@ public class SampleController implements Initializable {
         for (int i = 1; i < lineaTexto.size(); i++) {
             StringTokenizer sto = new StringTokenizer(lineaTexto.get(i).toString(), separador);
             int line = (lineaTexto.size() - 1);
-
+            
             for (int j = 0; j < contador; j++) { // imprimiendo todos los tokens de cada línea
                 if (j == 0) {
                     stringbufer.append("(");
                 }//cierrra for
 
-
+                
                 if (j != (contador - 1)) {
-
+                    
                     stringbufer.append(sto.nextToken().concat(","));
                 }//cierra if
                 else if (i != line) {
@@ -162,31 +164,17 @@ public class SampleController implements Initializable {
                 }
             }
             stringbufer.append("\r\n");
-
+            
         }
-
-
-
-
+        
+        
+        
+        textoGenerado = stringbufer.toString();
         System.out.println("" + stringbufer);
-
-//        FileChooser chooser = new FileChooser();
-//        File f = chooser.showSaveDialog(null);
-//        if (f != null) {
-//            FileWriter fw = new FileWriter(f);
-//            fw.write(String.valueOf(stringbufer));
-//            fw.close();
-
-        
-        
-//        }
-        
-
-
     }
-
+    
     public void checked(ActionEvent event) {
-
+        
         try {
             if (cbPeso.isSelected()) {
                 if (file.length() > 1000) {
@@ -203,15 +191,15 @@ public class SampleController implements Initializable {
         } catch (Exception e) {
             System.out.println("No se pudo determinar el peso del archivo por Ausencia o Daño en el mismo.");
         }
-
+        
     }
-
+    
     public void numeroLineas(ActionEvent event) throws FileNotFoundException, IOException {
         if (cbCantidad.isSelected()) {
             fr = new FileReader(file);
             bf = new BufferedReader(fr);
             long lNumeroLineas = 0;
-
+            
             while ((sCadena = bf.readLine()) != null) {
                 lNumeroLineas++;
                 System.out.println("" + sCadena);
@@ -221,7 +209,7 @@ public class SampleController implements Initializable {
             lblCantidad.setText("");
         }
     }
-
+    
     public void exit(ActionEvent event) {
         DialogResponse response = Dialogs.showConfirmDialog(null, "¿Desea salir de la aplicación?",
                 "SALIR", "Aviso importante", DialogOptions.OK_CANCEL);
@@ -229,11 +217,13 @@ public class SampleController implements Initializable {
             System.exit(0);
         }
     }
-
+    
     public void convert(ActionEvent event) throws FileNotFoundException, IOException {
+        
         buttonConvertir.setDisable(true);
         Scanner s = new Scanner(file);
-
+        
+        
         if (txtOtro.getText().isEmpty() == false) {
             separador = txtOtro.getText().trim();
             lblSeparador.setStyle("-fx-font-weight: bold; -fx-text-fill: #1E70AB;");
@@ -247,12 +237,17 @@ public class SampleController implements Initializable {
         } else if (grupo.getSelectedToggle().toString().equals("RadioButton[id=rbComa, styleClass=radio-button]")) {
             separador = ",";
         }
-
+        
         System.out.println("al final el separador sql es: " + separador);
-
+        
         readingtxt();
+        Toolkit toolkit = Toolkit.getDefaultToolkit();
+        Clipboard clipboard = toolkit.getSystemClipboard();
+        System.out.println(stringbufer.toString());
+        StringSelection strSel = new StringSelection(textoGenerado);
+        clipboard.setContents(strSel, null);
     }
-
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
